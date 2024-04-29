c83e39c749e1ed218dc434896027efca65c8fadc
WhiteCmile <whitecmile@WhiteCmile>
2024-04-29 16:02:59 +0800

    add a5

--- build/build.py
+++ build/build.py
@@ -22,10 +22,23 @@ f"""
 (load "src/schemer.scm")
 (load "{task}/{task}-wrapper.scm")
 
-(compiler-passes '(
-  verify-scheme
-  generate-x86-64
-))
+(compiler-passes `(
+                    verify-scheme
+                    uncover-frame-conflict
+                    introduce-allocation-forms
+                    (iterate
+                        select-instructions
+                        uncover-register-conflict
+                        assign-registers
+                        (break when everybody-home?)
+                        assign-frame
+                        finalize-frame-locations)
+                    discard-call-live
+                    finalize-locations
+                    expose-frame-var
+                    expose-basic-blocks
+                    flatten-program
+                    generate-x86-64))
 
 (load "{task}/tests{task_id}.scm")
 """)
