a7589d054cbc432bb406191e725623113841717c
WhiteCmile <whitecmile@WhiteCmile>
2024-05-03 18:21:46 +0800

    add flatten_program.scm

---
+++ src/flatten_program.scm
@@ -0,0 +1,19 @@
+(load "lib/match.scm")
+
+(define flatten-program
+    (lambda (program)
+        ; flatten label* and tail*
+        (define flatten_lbl_tail
+            (lambda (label* tail*)
+                (match label*
+                    [(,label ,labels* ...) (append (cons label (car tail*)) (flatten_lbl_tail (cdr label*) (cdr tail*)))]
+                    [,x '()])))
+        (define flatten_statement
+            (lambda (statement)
+                (match statement
+                    [(begin ,[flatten_statement -> effect*] ... ,[flatten_statement -> tail])
+                        (append (apply append effect*) tail)]
+                    [,x `(,x)])))
+        (match program
+            [(letrec ([,label* (lambda () ,[flatten_statement -> tail*])] ...) ,[flatten_statement -> body_tail])
+                (cons 'code (append body_tail (flatten_lbl_tail label* tail*)))])))
