bbc14732b2d8d141f27f32c3510023c65b4d6490
WhiteCmile <whitecmile@WhiteCmile>
2024-05-03 18:35:29 +0800

    finish modification of codegen.scm

--- src/codegen.scm
+++ src/codegen.scm
@@ -1,27 +1,32 @@
 (load "lib/match.scm")
 (load "lib/helpers.scm")
 (load "src/expose_frame_var.scm")
+(load "src/flatten_program.scm")
 
 (define generate-x86-64
     (lambda (program)
-        (match program
-            [(begin ,statement* ...) (codegen statement*)])))
-
-(define a1_handle_binop
-    (lambda (binop)
-        (match binop
-            [+ 'addq]
-            [- 'subq]
-            [* 'imulq])))
-
-(define a1_handle_statement
-    (lambda (statement)
-        (match statement
-            [(set! ,item1 (,binop ,item2 ,item3)) (emit (a1_handle_binop binop) item3 item1)]
-            [(set! ,item1 ,item2) (guard (or (int64? item2) (register? item2))) (emit 'movq item2 item1)])))
+        (let* ([expose_program (expose-frame-var program)]
+                [flatten_program (flatten-program expose_program)])
+            (match flatten_program
+                [(code ,statement* ...) (codegen statement*)]))))
 
 (define codegen
     (lambda (statements)
+        (define codegen_binop
+            (lambda (binop)
+                (match binop
+                    [+ 'addq]
+                    [- 'subq]
+                    [* 'imulq]
+                    [logand 'andq]
+                    [logor 'orq]
+                    [src 'sar])))
+        (define codegen_statement
+            (lambda (statement)
+                (match statement
+                    [,label (guard (label? label)) (emit-label label)]
+                    [(,label) (guard (label? label)) (emit-jump 'jump label)]
+                    [(set! ,item1 (,binop ,item2 ,item3)) (emit (codegen_statement binop) item3 item1)]
+                    [(set! ,item1 ,item2) (guard (or (int64? item2) (register? item2))) (emit 'movq item2 item1)])))
         (emit-program 
-            (for-each a1_handle_statement 
-                (expose-frame-var statements)))))
+            (for-each codegen_statement statements))))
