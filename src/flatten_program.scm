1b4bd545c47b28a526a297486e3be6fb5a756ac4
WhiteCmile <whitecmile@WhiteCmile>
2024-05-08 21:33:15 +0800

    rewrite flatten-program for a3

--- src/flatten_program.scm
+++ src/flatten_program.scm
@@ -2,19 +2,40 @@
 
 (define flatten-program
     (lambda (program)
-        ; flatten label* and tail*
-        (define flatten_lbl_tail
-            (lambda (label* tail*)
-                (match label*
-                    [(,label ,labels* ...) (append (cons label (car tail*)) (flatten_lbl_tail (cdr label*) (cdr tail*)))]
-                    [,x '()])))
-        (define flatten_statement
-            (lambda (statement)
-                (match statement
-                    [(begin ,[flatten_statement -> effect*] ... ,[flatten_statement -> tail])
-                        (append (apply append effect*) tail)]
-                    [(,triv) `((jump ,triv))]
-                    [,x `(,x)])))
+        (define Label
+            (lambda (labels tails)
+                (match labels
+                    [(,label . ,rest_labels)
+                        (let ([rest_program (Label rest_labels (cdr tails))]
+                                [instructions (cons label (Tail (car tails) rest_labels (cdr tails)))])
+                            (append instr rest_program))]
+                    [() '()])))
+        ; This function takes a tail, a list of labels, and a list of tails as arguments
+        ; Why it takes a list of labels? We need them to know how to generate the jump instructions for better performance
+        (define Tail
+            (lambda (tail labels tails)
+                (match tail
+                    [(begin ,effect* ... ,sub_tail)
+                        (append effect* (Tail sub_tail labels tails))]
+                    [(if ,operation (,label_true) (,label_false))
+                        (match labels
+                            [(,label . ,rest_labels)
+                                (let ([rest_program (Label labels tails)]
+                                        [instr 
+                                            (cond
+                                                [(eq? label_true label) `((if (not ,operation) (jump ,label_false)))]
+                                                [(eq? label_false label) `((if ,operation (jump ,label_true)))]
+                                                [else `((if ,operation (jump ,label_true)) (jump ,label_false))])])
+                                    (append instr rest_program))]
+                            [() `((if ,operation (jump ,label_true)) (jump ,label_false))])]
+                    [(,triv) (guard (label? triv))
+                        (match labels
+                            [(,label . ,rest_labels)
+                                (if (eq? label triv)
+                                    '()
+                                    `((jump ,triv)))]
+                            [() `(jump ,triv)])]
+                    [(,triv) `((jump ,triv))])))
         (match program
-            [(letrec ([,label* (lambda () ,[flatten_statement -> tail*])] ...) ,[flatten_statement -> body_tail])
-                (cons 'code (append body_tail (flatten_lbl_tail label* tail*)))])))
+            [(letrec ([,label* (lambda () ,tail*)] ...) ,body_tail)
+                (cons 'code (Tail body_tail label* tail*))])))
