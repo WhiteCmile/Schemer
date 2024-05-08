902e726f88bdb04b78b4b8975372789e69613c49
WhiteCmile <whitecmile@WhiteCmile>
2024-05-08 14:40:30 +0800

    forget to handle the case that "item2" in "(set! ,item1 ,item2)" is a label, which generates the "leaq" instruction instead of the "movq" a2 accomplished

--- src/codegen.scm
+++ src/codegen.scm
@@ -27,6 +27,7 @@
                     [,label (guard (label? label)) (emit-label label)]
                     [(jump ,triv) (emit-jump 'jmp triv)]
                     [(set! ,item1 (,binop ,item2 ,item3)) (emit (codegen_binop binop) item3 item1)]
+                    [(set! ,item1 ,item2) (guard (label? item2)) (emit 'leaq item2 item1)]
                     [(set! ,item1 ,item2) (emit 'movq item2 item1)])))
         (emit-program 
             (for-each codegen_statement statements))))
