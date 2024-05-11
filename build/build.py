036e649d02201b7d62cdfa5cad54d7500c29c8c9
WhiteCmile <whitecmile@WhiteCmile>
2024-05-11 22:08:56 +0800

    modify build.py; fix typo in helpers.scm

--- build/build.py
+++ build/build.py
@@ -22,6 +22,23 @@ f"""
 (load "src/schemer.scm")
 (load "{task}/{task}-wrapper.scm")
 
+(define-who everybody-home?
+    (define all-home?
+        (lambda (body)
+            (match body
+                [(locals (,local* ...)
+                    (ulocals (,ulocals* ...)
+                        (spills (,spill* ...)
+                            (locate (,home* ...)
+                                (frame-conflict, ct ,tail))))) #f]
+                [(locate (,home* ...) ,tail) #t]
+                [,x (error who "invalid Body ~s" x)])))
+    (lambda (x)
+        (match x
+            [(letrec ([,label* (lambda () ,body*)] ...) ,body)
+                (andmap all-home? `(,body ,body* ...))]
+            [,x (error who "invalid Program ~s" x)])))
+
 (compiler-passes '(
   verify-scheme
   generate-x86-64
