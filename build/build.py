8d610fe8c4450bea7bfbd24baab3a1fecc30d4d1
WhiteCmile <whitecmile@WhiteCmile>
2024-05-14 14:52:14 +0800

    modify build.py

--- build/build.py
+++ build/build.py
@@ -45,14 +45,15 @@ f"""
   flatten-set!
   impose-calling-conventions
   uncover-frame-conflict
-  introduce-allocation-forms
+  pre-assign-frame
+  assign-new-frame
   (iterate
+    finalize-frame-locations
     select-instructions
     uncover-register-conflict
     assign-registers
     (break when everybody-home?)
-    assign-frame
-    finalize-frame-locations)
+    assign-frame)
   discard-call-live
   finalize-locations
   expose-frame-var
