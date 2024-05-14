e417ac23e1fd142e1c93ff1e628f33c51415348d
WhiteCmile <whitecmile@WhiteCmile>
2024-05-14 21:02:44 +0800

    rewrite expose-frame-var to handle incresement and decresement of frame-pointer-register

--- src/expose_frame_var.scm
+++ src/expose_frame_var.scm
@@ -1,35 +1,50 @@
 (load "lib/match.scm")
 
-(define expose-frame-var
-    (letrec 
-        ([handle_statement 
-            (lambda (statement)
-                (match statement
-                    [(begin ,[handle_statement -> effect*] ... ,[handle_statement -> tail]) `(begin ,effect* ... ,tail)]
-                    [(if ,[handle_statement -> stat1] ,[handle_statement -> stat2] ,[handle_statement -> stat3])
-                        `(if ,stat1 ,stat2 ,stat3)]
-                    [(set! ,[handle_var -> var1] (,binop ,[handle_triv -> triv1] ,[handle_triv -> triv2])) 
-                        `(set! ,var1 (,binop ,triv1 ,triv2))]
-                    [(set! ,[handle_var -> var] ,[handle_triv -> triv]) `(set! ,var ,triv)]
-                    [(return-point ,label ,[handle_statement -> tail])
-                        `(return-point ,label ,tail)]
-                    [(,relop ,[handle_triv -> triv1] ,[handle_triv -> triv2])
-                        (guard (relop? relop))
-                        `(,relop ,triv1 ,triv2)]
-                    [(,triv) (guard (triv? triv)) (list (handle_triv triv))]
-                    [,x x]))]
-        [handle_var
-            (lambda (var)
-                (if (register? var)
-                    var
-                    (make-disp-opnd frame-pointer-register (ash (frame-var->index var) align-shift))))]
-        [handle_triv
-            (lambda (triv)
-                (match triv
-                    [,x (guard (int64? x)) x]
-                    [,x (guard (label? x)) x]
-                    [,x (handle_var x)]))])
-        (lambda (program)
-            (match program
-                [(letrec ([,label* (lambda () ,[handle_statement -> tail*])] ...) ,[handle_statement -> body_tail])
-                    `(letrec ([,label* (lambda () ,tail*)] ...) ,body_tail)]))))
+(define (expose-frame-var program)
+    (define fp frame-pointer-register)
+    (define (Body tail)
+        (define rbp_offset 0)
+        (define (Stats statements)
+            (match statements
+                [() '()]
+                [(,statement . ,rest)
+                    (let* 
+                        ([statement (Stat statement)]
+                        [rest (Stats rest)])
+                    (cons statement rest))]))
+        (define (Stat statement)
+            (match statement
+                [(begin ,stat* ...) 
+                    (let ([stats (Stats stat*)]) 
+                        `(begin ,@stats))]
+                [(if ,stat* ...) 
+                    (let ([stats (Stats stat*)])
+                        `(if ,@stats))]
+                [(set! ,[Var -> var1] (,binop ,[Triv -> triv1] ,[Triv -> triv2])) 
+                    (if (eq? var1 fp)
+                        (match binop
+                            [+ (set! rbp_offset (+ rbp_offset triv2))]
+                            [- (set! rbp_offset (- rbp_offset triv2))]))
+                    `(set! ,var1 (,binop ,triv1 ,triv2))]
+                [(set! ,[Var -> var] ,[Triv -> triv]) `(set! ,var ,triv)]
+                [(return-point ,label ,[Stat -> tail])
+                    `(return-point ,label ,tail)]
+                [(,relop ,[Triv -> triv1] ,[Triv -> triv2])
+                    (guard (relop? relop))
+                    `(,relop ,triv1 ,triv2)]
+                [(,triv) (guard (triv? triv)) (list (Triv triv))]
+                [,x x]))
+        (define (Var var)
+            (if (register? var)
+                var
+                (make-disp-opnd fp (- (ash (frame-var->index var) align-shift) rbp_offset))))
+        (define (Triv triv)
+            (match triv
+                [,x (guard (int64? x)) x]
+                [,x (guard (label? x)) x]
+                [,x (Var x)]))
+        (match tail
+            [,[Stat -> tail] tail]))
+    (match program
+        [(letrec ([,label* (lambda () ,[Body -> tail*])] ...) ,[Body -> body_tail])
+            `(letrec ([,label* (lambda () ,tail*)] ...) ,body_tail)]))
