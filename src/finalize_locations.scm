5724869c5b16a6f15aeb719bc5c32afb00f40139
WhiteCmile <whitecmile@WhiteCmile>
2024-05-15 11:21:21 +0800

    modify finalize-locations to handle "(mref)" and "(mset!)"

--- src/finalize_locations.scm
+++ src/finalize_locations.scm
@@ -10,12 +10,16 @@
                 [(if ,[pred] ,[stat1] ,[stat2]) `(if ,pred ,stat1 ,stat2)]
                 [(,relop ,triv1 ,triv2) (guard (relop? relop))
                     `(,relop ,(replace triv1) ,(replace triv2))]
+                [(set! ,[var] (mref ,[triv1] ,[triv2]))
+                    `(set! ,var (mref ,triv1 ,triv2))]
                 [(set! ,[var] (,binop ,[triv1] ,[triv2])) 
                     `(set! ,var (,binop ,triv1 ,triv2))]
                 [(set! ,[var] ,[triv])
                     (if (eq? var triv)
                         '(nop)
                         `(set! ,var ,triv))]
+                [(mset! ,[base] ,[offset] ,[value])
+                    `(mset! ,base ,offset ,value)]
                 [(,triv) (guard (triv? triv)) `(,(replace triv))]
                 [(,proc ,triv* ...) (guard (triv? proc)) `(,proc ,(replace triv*) ...)]
                 [(return-point ,label ,[tail]) `(return-point ,label ,tail)]
