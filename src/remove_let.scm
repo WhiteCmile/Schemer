bf6656d42f07e6dae19a84042104b8f6d96c325b
WhiteCmile <whitecmile@WhiteCmile>
2024-05-16 21:25:01 +0800

    add remove-let; Unoptimized

---
+++ src/remove_let.scm
@@ -0,0 +1,19 @@
+(define (remove-let program)
+    ; Stat handle Tail, Effect, Pred and Value structures
+    (define (Stat statement)
+        (match statement
+            [(begin ,[statement*] ...) (make-begin statement*)]
+            [(if ,[statement*] ...) (if ,statement* ...)]
+            [(mset! ,[value*] ...) `(mset! ,value* ...)]
+            [(alloc ,[value]) `(alloc ,value)]
+            [(let ,binding* ,[statement])
+                (make-begin 
+                    `(,(map (lambda (binding) 
+                            `(set! ,(car binding) ,(cadr binding))) binding*)
+                        ,statement))]
+            [(,[value*] ...) `(,value* ...)]
+            [,x x]))
+    (match program
+        [(letrec ([,label* (lambda ,uvar** ,[body*])] ...) ,[body])
+            `(letrec ([,label* (lambda ,uvar** ,body*)] ...) ,body)]
+        [(locals ,uvar* ,[Stat -> tail]) `(locals ,uvar* ,tail)]))
