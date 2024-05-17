03ce0c399486016a02f1fbaab05170a16cb5456f
WhiteCmile <whitecmile@WhiteCmile>
2024-05-17 10:33:36 +0800

    update specify-representation; add handlement of immediate types

---
+++ src/specify_representation.scm
@@ -0,0 +1,64 @@
+; Check whether a prim is a value-prim
+(define (value-prim? prim)
+    (match prim
+        [make-vector #t]
+        [vector-length #t]
+        [vector-ref #t]
+        [void #t]
+        [,x (binop? x)]))
+
+; Check whether a prim is a pred-prim
+(define (pred-prim? prim)
+    (match prim
+        [boolean? #t]
+        [eq? #t]
+        [fixnum? #t]
+        [null? #t]
+        [pair? #t]
+        [vector? #t]
+        [,x (relop? x)]))
+
+; Check whether a prim is an effect-prim
+(define (effect-prim? prim)
+    (match prim
+        [set-car! #t]
+        [set-cdr! #t]
+        [vector-set! #t]))
+
+; Specify the representation of an immediate
+(define (specify_immediate statement)
+    (match statement
+        [(quote #f) $false]
+        [(quote #t) $true]
+        [(quote ()) $nil]
+        [(quote ,x) (guard (fixnum? x)) (ash x shift-fixnum)]))
+
+(define (specify_binop op values)
+    (match op
+        [+ `(+ ,@values)]
+        [- `(- ,@values)]
+        [* `(* ,(car values) (sra ,(cadr values) ,shift-fixnum))]
+        [,x (error "Invalid binop ~s\n" x)]))
+
+; Specify the representation of a value prim
+; prim is the prim, and values is a list of specified values
+(define (specify_value_prim prim values)
+    (match prim
+        [void `(,$void)]
+        [,op (guard (binop? op)) (specify_binop op values)]))
+
+(define (specify-representation program)
+    ; The Stmt helper handles Value, Pred and Effect structures
+    (define (Stmt statement)
+        (match statement
+            [(begin ,[Stmt -> statement*] ...) `(begin ,statement* ...)]
+            [(if ,[Stmt -> statement*] ...) `(if ,statement* ...)]
+            [(let ([,uvar* ,[Stmt -> statement*]] ...) ,[Stmt -> statement])
+                `(let ([,uvar* ,statement*] ...) ,statement)]
+            [(quote ,immediate) (specify_immediate statement)]
+            [(,prim ,[Stmt -> value*] ...) (guard (value-prim? prim)) 
+                (specify_value_prim prim value*)]))
+            
+    (match program
+        [(letrec ([,label* (lambda ,uvar** ,[Stmt -> value*])] ...) ,[Stmt -> value])
+            `(letrec ([,label* (lambda ,uvar** ,value*)] ...) ,value)]))
