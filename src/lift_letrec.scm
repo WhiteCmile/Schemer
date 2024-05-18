0f4515f4c38872754b6138216161221b6cc83f90
WhiteCmile <whitecmile@WhiteCmile>
2024-05-18 12:01:43 +0800

    add lift-letrec

---
+++ src/lift_letrec.scm
@@ -0,0 +1,26 @@
+(define (lift-letrec program)
+    (define (Expr expr)
+        (match expr
+            [(let 
+                ([,uvar* ,[Expr -> label** lambda** expr*]] ...)
+                ,[Expr -> label* lambda* sub_expr])
+                (values 
+                    `(,@(apply append label**) ,@label*)
+                    `(,@(apply append lambda**) ,@lambda*)
+                    `(let ([,uvar* ,expr*] ...) ,sub_expr))]
+            [(letrec
+                ([,label* (lambda ,uvar** ,[Expr -> label** lambda** expr*])] ...)
+                ,[Expr -> sub_label* sub_lambda* sub_expr])
+                (values
+                    `(,@label* ,@(apply append label**) ,@sub_label*)
+                    `(,@([lambda ,uvar** ,expr*] ...) ,@(apply append lambda**) ,@sub_lambda*)
+                    sub_expr)]
+            [(,[Expr -> label** lambda** ,expr*] ...)
+                (values
+                    (apply append label**)
+                    (apply append lambda**)
+                    `(,@expr*))]
+            [,x (values '() '() x)])) 
+    (let-values 
+        ([(labels lambdas expr) (Expr program)])
+        `(letrec ([,labels ,lambdas] ...) ,expr)))
