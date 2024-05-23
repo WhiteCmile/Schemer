e38ff7f080d39715b1f22bacfabe5df197df8b11
WhiteCmile <whitecmile@WhiteCmile>
2024-05-23 22:52:09 +0800

    add uncover-free

---
+++ src/uncover_free.scm
@@ -0,0 +1,43 @@
+(define (uncover-free expr)
+    (define (Lambda lambda_expr)
+        (match lambda_expr
+            [(lambda ,uvar* ,expr)
+                (let-values 
+                    ([(frees expr) ((Expr uvar*) expr)])
+                    `(lambda ,uvar*
+                        (free ,frees
+                            ,expr)))]))
+    (define (Expr locals)
+        (lambda (expr)
+            (match expr
+                [(quote ,imm) (values '() `(quote ,imm))]
+                [(begin ,[frees* expr*] ...)
+                    (values (apply append frees*)
+                        `(begin ,expr* ...))]
+                [(if ,[frees* expr*] ...)
+                    (values (apply append frees*)
+                        `(if ,expr* ...))]
+                [(let ([,uvar* ,[bind_frees* bind_expr*]] ...) ,expr)
+                    (let-values 
+                        ([(frees sub_expr) ((Expr (union locals uvar*)) expr)])
+                        (values (union frees (apply append bind_frees*)))
+                            `(let ([,uvar* ,bind_expr*] ...)
+                                ,sub_expr))]
+                [(letrec ([,uvar* ,[Lambda -> lambda_expr*]] ...)
+                    ,[frees sub_expr])
+                    (values frees
+                        `(letrec ([,uvar* ,lambda_expr*] ...)
+                            ,sub_expr))]
+                [(,prim ,[frees* expr*] ...) (guard (prim? prim))
+                    (values (apply append frees*)
+                        `(,prim ,expr* ...))]
+                [(,[frees* expr*] ...)
+                    (values (apply append frees*)
+                        `(,expr* ...))]
+                [,uvar 
+                    (if (member uvar locals)
+                        (values '() uvar)
+                        (values `(,uvar) uvar))])))
+    (let-values     
+        ([(uvars expr) ((Expr '()) expr)])
+        expr))
