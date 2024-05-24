0f635fa8c8c752eb7e76d2ebfb638dbccd6bb61e
WhiteCmile <whitecmile@WhiteCmile>
2024-05-24 19:32:42 +0800

    add sanitize-binding-forms

---
+++ src/sanitize_binding_forms.scm
@@ -0,0 +1,33 @@
+(define (sanitize-binding-forms program)
+    (define (extract_lambda_bindings uvar* expr* sub_expr)
+        (define let_bindings '())
+        (define letrec_bindings '())
+        (let loop 
+            ([uvar* uvar*] [expr* expr*])
+            (match uvar*
+                [() '()]
+                [(,uvar . ,rest)
+                    (match (car expr*)
+                        [(lambda ,params* ,[sanitize-binding-forms lambda_body])
+                            (set! letrec_bindings 
+                                (cons
+                                    (list uvar `(lambda ,params* ,lambda_body))
+                                    letrec_bindings))]
+                        [,x (set! let_bindings (cons (list uvar x) let_bindings))])
+                    (loop rest (cdr expr*))]))
+        (cond
+            [(null? let_bindings) `(letrec ,letrec_bindings ,sub_expr)]
+            [(null? letrec_bindings) `(let ,let_bindings ,sub_expr)]
+            [else `(letrec ,letrec_bindings
+                        (let ,let_bidings ,sub_expr))]))
+    (match program
+        [(begin ,[expr*] ...) (make-begin expr*)]
+        [(if ,[expr*] ...) `(if ,@expr*)]
+        [(letrec () ,[sub_expr]) sub_expr]
+        [(let () ,[sub_expr]) sub_expr]
+        [(letrec ([,uvar* (lambda ,params* ,[lambda_body*])] ...) ,[sub_expr])
+            `(letrec ([,uvar* (lambda ,params* ,lambda_body*)] ...) ,sub_expr)]
+        [(let ([,uvar* ,expr*]) ,[sub_expr])
+            (extract_lambda_bindings uvar* expr* sub_expr)]
+        [(,[expr*] ...) `(,@expr*)]
+        [,x x]))
