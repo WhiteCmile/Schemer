e836e3a377939d3152512f6b47009d26f6c1e263
WhiteCmile <whitecmile@WhiteCmile>
2024-05-24 20:01:26 +0800

    add optimize-known-call

---
+++ src/optimize_known_call.scm
@@ -0,0 +1,34 @@
+(define (optimize-known-call program)
+    (define (Expr known_closures can_replace)
+        (define Proc (Expr known_closures #t))
+        (define NotProc (Expr known_closures #f))
+        (lambda (expr)
+            (match expr
+                [(quote ,imm) `(quote ,imm)]
+                [(begin ,[NotProc -> expr*] ... ,[tail]) `(begin ,@expr* ,tail)]
+                [(if ,[NotProc -> pred] ,[conseq] ,[alter]) `(if ,pred ,conseq ,alter)]
+                [(let ([,uvar* ,[NotProc -> expr*]] ...) ,[sub_expr])
+                    `(let ([,uvar* ,expr*] ...) ,sub_expr)]
+                [(letrec ([,uvar* ,expr*] ...)
+                    (closures ,closure* ...
+                        ,sub_expr))
+                    (let*
+                        ([known_closures 
+                            (append known_closures 
+                                (map (lambda (closure_vars)
+                                        (list (car closure_vars) (cadr closure_vars)))
+                                    closure*))]
+                        [expr* (map (Expr known_closures #f) expr*)])
+                        `(letrec ([,uvar* ,expr*] ...) 
+                            ,((Expr known_closures can_replace) sub_expr)))]
+                [(,prim ,[NotProc -> expr*] ...) (guard (prim? prim))
+                    `(,prim ,@expr*)]
+                [(,[Proc -> proc] ,[NotProc -> expr*] ...)
+                    `(,proc ,@expr*)]
+                [,uvar (guard (uvar? uvar))
+                    (let ([closure (assoc uvar known_closures)])
+                        (if closure
+                            (cadr closure)
+                            uvar))]
+                [,x (error 'Expr "Unknown expression ~s\n" expr)])))
+    ((Expr '() #f) program))
