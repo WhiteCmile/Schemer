3113459ca4bd2ee589ca6e030cee25d396797ab3
WhiteCmile <whitecmile@WhiteCmile>
2024-05-24 10:18:13 +0800

    add convert-closures

---
+++ src/convert_closures.scm
@@ -0,0 +1,36 @@
+(define (convert-closures program)
+    (define (Lambda lambda_expr)
+        (printf "lambda: ~a\n" lambda_expr)
+        (match lambda_expr
+            [(,uvar 
+                (lambda ,param*
+                    (free ,free*
+                        ,[Expr -> expr])))
+                (let 
+                    ([label (unique-label uvar)]
+                    [cp (unique-name 'cp)])
+                    (values 
+                        `(,uvar ,label ,@param*)
+                        `(,label
+                            (lambda (,cp ,@param*)
+                                (bind-free (,cp ,@free*)
+                                    ,expr)))))]))
+    (define (Expr expr)
+        (match expr
+            [(quote ,imm) `(quote ,imm)]
+            [(if ,[expr*] ...) `(if ,expr* ...)]
+            [(begin ,[expr*] ...) `(begin ,expr* ...)]
+            [(let ([,uvar* ,[expr*]] ...) ,[sub_expr])
+                `(let ([,uvar* ,expr*] ...) ,sub_expr)]
+            [(letrec 
+                (,[Lambda -> closure* lambda_expr*] ...)
+                ,[sub_expr])
+                `(letrec
+                    (,lambda_expr* ...)
+                        (closures ,closure* ,sub_expr))]
+            [(,prim ,[expr*] ...) (guard (prim? prim))
+                `(,prim ,expr* ...)]
+            [(,[proc] ,[expr*] ...) 
+                `(,proc ,proc ,expr* ...)]
+            [,x x]))
+    (Expr program))
