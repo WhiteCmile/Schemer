f8f51f692a4d2357ec8872841e23a7dfa51c7fea
WhiteCmile <whitecmile@WhiteCmile>
2024-05-24 14:33:36 +0800

    merge correct version of optimize-jump from a11

--- src/optimize_jumps.scm
+++ src/optimize_jumps.scm
@@ -5,20 +5,20 @@
                 [() (values '() '() '())]
                 [(,label . ,rest)
                     (let-values 
-                        ([(label* tail* assoc_list) (loop rest (cdr tail*))])
+                        ([(new_label* new_tail* assoc_list) (loop rest (cdr tail*))])
                         (match (car tail*)
                             [(,jump_label) (guard (label? jump_label))
-                                (values label* tail* (cons (cons label jump_label) assoc_list))]
-                            [,x (values (cons label label*) (cons x tail*) assoc_list)]))])))
+                                (values new_label* new_tail* (cons (list label jump_label) assoc_list))]
+                            [,x (values (cons label new_label*) (cons x new_tail*) assoc_list)]))])))
     (define (optimize_tail assoc_list)
         (lambda (tail)
             (match tail
                 [(,[item*] ...) `(,@item*)]
                 [,label (guard (label? label))
                     (let ([jump_label (assoc label assoc_list)])
-                        (if (null? jump_label)
-                            label
-                            (cadr jump_label)))]
+                        (if jump_label
+                            ((optimize_tail assoc_list) (cadr jump_label))
+                            label))]
                 [,x x])))
     (match program
         [(letrec ([,label* (lambda () ,tail*)] ...)
