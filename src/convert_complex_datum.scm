bb7f17ba5e775b6e46b409fb48814b7faa877fe5
WhiteCmile <whitecmile@WhiteCmile>
2024-05-26 18:37:51 +0800

    rewrite convert-complex-datum

--- src/convert_complex_datum.scm
+++ src/convert_complex_datum.scm
@@ -4,9 +4,18 @@
     ;   2. Complex datum values
     ;   3. Set expressions to initialize vector datum
     ;   4. Program with complex datum replaced by new variables
+
+    ; All vector will be transformed into a new variable
+    ; while all pairs will be the form of (cons car cdr)
     (define (handle_quoted_data data)
+        ; Integrate all sub expressions to make a new data_expr
+        (define (integrate_sub_exprs data_expr new_vars new_values set_exprs)
+            (if (null? new_vars)
+                data_expr
+                `(let ,(map list new_vars new_values)
+                    ,(make-begin `(,@set_exprs ,data_expr)))))
         ; Transform the pair into (cons car cdr) form
-        (define (flatten_pair pair)
+        (define (handle_pair pair)
             (match pair
                 [,x (guard (null? x)) 
                     (values '() '() '() ''())]
@@ -14,46 +23,43 @@
                     (handle_quoted_data x)]
                 [,pair 
                     (let-values 
-                        ([(car_new_vars car_values car_set_exprs car_expr) (flatten_pair (car pair))]
-                        [(cdr_new_vars cdr_values cdr_set_exprs cdr_expr) (flatten_pair (cdr pair))])
-                        (let ([new_vars (append car_new_vars cdr_new_vars)]
-                            [new_values (append car_values cdr_values)]
-                            [new_set_exprs (append car_set_exprs cdr_set_exprs)])
-                            (values '() '() '() 
-                                (if (null? new_vars)
-                                    `(cons ,car_expr ,cdr_expr)
-                                    `(let ,(map list new_vars new_values)
-                                        ,(make-begin 
-                                            `(,@new_set_exprs (cons ,car_expr ,cdr_expr))))))))]))
+                        ([(car_new_uvars car_values car_set_exprs car_expr) (handle_pair (car pair))]
+                        [(cdr_new_uvars cdr_values cdr_set_exprs cdr_expr) (handle_pair (cdr pair))])
+                        (values '() '() '()
+                            (integrate_sub_exprs 
+                                `(cons ,car_expr ,cdr_expr)
+                                (append car_new_uvars cdr_new_uvars)
+                                (append car_values cdr_values)
+                                (append car_set_exprs cdr_set_exprs))))]))
+        ; Get the expressions to initialize vector datum
         (define (set_vector vec vec_name)
             (let loop ([vec vec] [i 0])
                 (match vec
-                    [() (values '() '() '() '())]
+                    [() '()]
                     [(,elem . ,rest)
                         (let-values
-                            ([(rest_new_vars rest_values rest_set_exprs sub_set_exprs) 
-                                (loop rest (add1 i))]
-                            [(elem_new_vars elem_values elem_set_exprs elem_expr) 
+                            ([(elem_new_vars elem_values elem_set_exprs elem_expr) 
                                 (handle_quoted_data elem)])
-                            (values
-                                (append elem_new_vars rest_new_vars)
-                                (append elem_values rest_values)
-                                (cons `(vector-set! ,vec_name (quote ,i) ,elem_expr) rest_set_exprs)
-                                (append elem_set_exprs sub_set_exprs)))])))
+                            (cons 
+                                `(vector-set! ,vec_name (quote ,i) 
+                                    ,(integrate_sub_exprs
+                                        elem_expr
+                                        elem_new_vars
+                                        elem_values
+                                        elem_set_exprs))
+                                (loop rest (add1 i))))])))
         (define (handle_vector vec)
             (let ([new_var (unique-name 'vec)])
-                (let-values 
-                    ([(new_vars datum_values cur_set_exprs sub_set_exprs) (set_vector vec new_var)])
-                    (values `(,new_var)
-                            `((make-vector (quote ,(length vec))))
-                            cur_set_exprs
-                            (if (null? new_vars)
-                                new_var
-                                `(let ,(map list new_vars datum_values)
-                                    ,(make-begin `(,@sub_set_exprs new_var))))))))
+                (let
+                    ([set_exprs (set_vector vec new_var)])
+                    (values 
+                        `(,new_var) 
+                        `((make-vector (quote ,(length vec))))
+                        set_exprs
+                        new_var))))
         (match data
             [#(,elem* ...) (handle_vector elem*)]
-            [,pair (guard (pair? pair)) (flatten_pair pair)]
+            [,pair (guard (pair? pair)) (handle_pair pair)]
             [,x (values '() '() '() `(quote ,x))]))
     ; Handle expressions
     (define (Expr expr)
@@ -61,14 +67,11 @@
             [(quote ,data) 
                 (let-values 
                     ([(new_vars datum_values set_exprs data_expr) (handle_quoted_data data)])
-                    (cond 
-                        [(pair? data) 
-                            (let ([new_var (unique-name 'pair)])
-                                (values (append new_vars `(,new_var))
-                                        (append datum_values `(,data_expr))
-                                        set_exprs
-                                        new_var))]
-                        [else (values new_vars datum_values set_exprs data_expr)]))]
+                    ; If the returned data is a pair, we need to bind it to a new variable
+                    (if (pair? data)
+                        (let ([new_var (unique-name 'cmplx_datum)])
+                            (values `(,new_var) `(,data_expr) '() new_var))
+                        (values new_vars datum_values set_exprs data_expr)))]
             [(,[Expr -> new_vars* values* set_exprs* expr*] ...)
                 (values (apply append new_vars*)
                         (apply append values*)
@@ -78,4 +81,4 @@
     (let-values
         ([(new_vars datum_values set_exprs expr) (Expr program)])
         `(let ,(map list new_vars datum_values)
-            ,(make-begin (append set_exprs `(,expr))))))
+            ,(make-begin `(,@set_exprs ,expr)))))
